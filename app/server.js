e7f87b72ed0e301bdfbf60a937bd5a7ea99bdc9c
Vishaldk18 <vishaldgk18@gmail.com>
2026-01-11 17:33:17 +0000

    Intial Task Manager API

---
+++ app/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://mongo:27017/taskdb");
+
+const Task = mongoose.model('Task', {
+    title: String,
+    completed: Boolean
+});
+
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.json(task);
+});
+
+app.put('/tasks/:id', async (req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(task);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Deleted' });
+});
+
+app.listen(3000, () => console.log("API running on port 3000"));
